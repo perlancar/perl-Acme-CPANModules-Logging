cd447ea77f3946841310b33477e22020acb6ac8c
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-04-09 09:27:25 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/Logging.pm
@@ -0,0 +1,17 @@
+package Acme::CPANModules::Logging;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "Logging frameworks on CPAN",
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 SEE ALSO
